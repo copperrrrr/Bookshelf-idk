73a3bb9c83f910e97dfc6f4ed326fd1fd6555bdb
Nick <nikita.mitrohin.08@gmail.com>
2025-10-17 16:35:31 +0300

    A base class for all products is described

---
+++ Base.h
@@ -0,0 +1,30 @@
+#ifndef BASE_H
+#define BASE_H
+#include <string>
+#include <iostream>
+class Base {
+protected:
+    double price;
+
+public: 
+    Base();
+    Base(double p);
+    Base(const Base& other);
+
+    virtual ~Base();
+
+    virtual void display() const = 0;
+    virtual std::string getType() const = 0;
+    virtual void SaveToFile(std::ofstream& out) const = 0;
+    virtual void LoadFromFile(std::ifstream& in) const = 0;
+    virtual void edit() const = 0;
+
+    double getPrice() const;
+    void setPrice(double p);
+
+    Base& operator=(const Base& other);
+    friend std::ostream& operator<<(std::ostream& os, const Base& obj);
+};
+
+#endif
+
